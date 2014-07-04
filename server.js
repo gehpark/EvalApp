91e015deed194e4694bf0cae62dc440f01447243
gehpark <gehpark@gmail.com>
2014-07-04 14:49:49 +0900

    first copy

---
+++ server.js
@@ -0,0 +1,9 @@
+// ========= SEVER PROTOTYPE =========//
+
+//create a client prototype
+exports.Server = function () {
+  // create an array to hold all its received requests
+  this.reqs = [];
+  // create a peer element to represent the client for WebRTC
+  // this.peer = new Peer(id, {key: 'myapikey'}); 
+}
